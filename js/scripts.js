d59296bdb1bdb8d99f2c957fbbbd3e9268a5e4f9
Rodrigo Ruiz <ruiz.rodrigo46@yahoo.com>
2021-05-27 10:30:27 -0400

    added shopping list exercise

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const generateListButton = document.querySelector('#generateList');
+const myList = document.getElementById('myList');
+
+
+generateListButton.addEventListener('click', function() {
+    const allInputs = document.querySelectorAll('input');
+    const list = document.createElement('ul');
+
+    allInputs.forEach(function program(item) {
+        const li = document.createElement('li');
+        li.innerText = item.value;
+        list.append(li);
+        
+    });
+    myList.append(list);
+
+});
